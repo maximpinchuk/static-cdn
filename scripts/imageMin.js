16d2f47eb2e532acca095030e0063d210fb0630e
Maxim Pinchuk <maxim.pinchuk@superdao.co>
2022-10-05 15:22:25 +0400

    Initial commit

---
+++ scripts/imageMin.js
@@ -0,0 +1,46 @@
+const compress_images = require("compress-images");
+
+compress_images(
+  'static/**/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}',
+  'dist/',
+  {
+    compress_force: false,
+    statistic: true,
+    autoupdate: true
+  },
+  false,
+  {
+    jpg: {
+      engine: "mozjpeg",
+      command: ["-quality", "60"]
+    }
+  },
+  {
+    png: {
+      engine: "pngquant",
+      command: ["--quality=20-50", "-o"]
+    }
+  },
+  {
+    svg: {
+      engine: "svgo",
+      command: "--multipass"
+    }
+  },
+  {
+    gif: {
+      engine: "gifsicle",
+      command: ["--colors", "64", "--use-col=web"]
+    }
+  },
+  function (error, completed, statistic) {
+    console.log("-------------");
+    if (error) {
+      console.log(error);
+    }
+    if (statistic) {
+      console.log(statistic);
+    }
+    console.log("-------------");
+  }
+);
